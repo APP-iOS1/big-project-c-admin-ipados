3c11ccdc0072066d91781c4a511014e38999d812
ryuchanghwi <ckdgnl10@naver.com>
2022-12-27 17:39:08 +0900

    loginView UI

---
+++ big_project_c_admin_ipados/big_project_c_admin_ipados/ViewModel/LoginViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  LoginViewModel.swift
+//  big_project_c_admin_ipados
+//
+//  Created by 류창휘 on 2022/12/27.
+//
+
+import Foundation
+
+class LoginViewModel : ObservableObject {
+    @Published var checkedLoginForm : Bool = false
+    
+    func checkEmailRule(email : String) -> Bool {
+        let regExp = #"^[a-zA-Z0-9+-\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]{2,3}+$"#
+        return email.range(of: regExp, options: .regularExpression) != nil
+    }
+    func checkPasswordRule(password : String) -> Bool {
+        let regExp = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+=-]).{8,20}$"
+        return password.range(of: regExp, options: .regularExpression) != nil
+    }
+}
