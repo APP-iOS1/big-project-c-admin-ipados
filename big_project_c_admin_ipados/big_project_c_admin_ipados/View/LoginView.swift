5019ee161f224b5739c4673581f0ea107c5b6ca9
thekoon0456 <thekoon0456@gmail.com>
2022-12-27 18:34:31 +0900

    fix loginView

--- big_project_c_admin_ipados/big_project_c_admin_ipados/View/LoginView.swift
+++ big_project_c_admin_ipados/big_project_c_admin_ipados/View/LoginView.swift
@@ -109,17 +109,7 @@ struct LoginView_Previews: PreviewProvider {
         LoginView()
     }
 }
-extension Color {
-    // MARK: hex code를 이용하여 정의
-    // ex. UIColor(hex: 0xF5663F)
-    init(hex: UInt) {
-        self.init(
-            red: CGFloat((hex & 0xFF0000) >> 16) / 255.0,
-            green: CGFloat((hex & 0x00FF00) >> 8) / 255.0,
-            blue: CGFloat(hex & 0x0000FF) / 255.0
-        )
-    }
-}
+
 ////https://www.youtube.com/watch?v=2Jk58S6FiZw
 //@State private var isLoadingIndicator : Bool = false
 //var body: some View {
