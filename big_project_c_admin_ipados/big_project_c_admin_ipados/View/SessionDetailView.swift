fbf8205a18dd3710ec6eab719c9d844d781cffa5
Lianne <yenchoi010@gmail.com>
2022-12-27 17:34:38 +0900

    Fix: GeneralView + SessionDetailView

--- big_project_c_admin_ipados/big_project_c_admin_ipados/View/SessionDetailView.swift
+++ big_project_c_admin_ipados/big_project_c_admin_ipados/View/SessionDetailView.swift
@@ -11,6 +11,8 @@ struct SessionDetailView: View {
     @ObservedObject var euni: EuniStore
 
     var euniId: Euni.ID?
+    @State private var clickedEditButton: Bool = false
+    @State private var clickedQRButton: Bool = false
     
     var selectedContent: Euni? {
         get {
@@ -24,19 +26,63 @@ struct SessionDetailView: View {
     }
     
     var body: some View {
-        VStack {
+        VStack(alignment: .leading) {
             Text(selectedContent?.title ?? "")
+                .font(.title)
+                .fontWeight(.bold)
+            HStack {
+                HStack {
+                    Image(systemName: "calendar")
+                    Text(selectedContent?.time ?? "")
+                    Image(systemName: "mappin.and.ellipse")
+                    Text(selectedContent?.location ?? "")
+                }
+                .font(.caption2)
+                
+                Spacer()
+                
+                HStack {
+                    Button {
+                        // TODO: 내용 수정 기능 구현
+                        clickedEditButton.toggle()
+                    } label: {
+                        Text("세미나 내용 수정하기")
+                            .frame(width: 150)
+                            .padding(12)
+                            .fontWeight(.bold)
+                            .foregroundColor(clickedEditButton ? Color.accentColor : Color.white)
+                            .background(clickedEditButton ? Color.white : Color.accentColor)
+                            .cornerRadius(15)
+                    }
+                    
+                    Button {
+                        // TODO: QR코드 연결
+                        clickedQRButton.toggle()
+                    } label: {
+                        Text("QR코드")
+                            .frame(width: 150)
+                            .padding(12)
+                            .fontWeight(.bold)
+                            .foregroundColor(clickedQRButton ? Color.accentColor : Color.white)
+                            .background(clickedQRButton ? Color.white : Color.accentColor)
+                            .cornerRadius(15)
+                    }
+
+                }
+            }
             
-//            List(euni.eunis) { sample in
-//                Text(sample.lecturer)
-//                Text(sample.title)
-//                    .font(.title)
-//                HStack(spacing: 10) {
-//                    Text(sample.time)
-//                    Text(sample.location)
-//                }
-//            }
+            Divider()
+            
+
+            
+            Text("받은 Q&A")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            
+
+            Spacer()
         }
+        .padding(.horizontal, 80)
     }
 }
 
