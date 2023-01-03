3098a9c69b91ef727f2eae485f1fc63d93735c62
FrediOS-dev <114239407+FrediOS-dev@users.noreply.github.com>
2023-01-03 18:06:48 +0900

    fix merge

--- big_project_c_admin_ipados/big_project_c_admin_ipados/View/SessionDetailView.swift
+++ big_project_c_admin_ipados/big_project_c_admin_ipados/View/SessionDetailView.swift
@@ -147,104 +147,22 @@ struct SessionDetailView: View {
                                 questionStore.fetchQuestion(seminarID: seminarId ?? "")
                             }
                             
-                            
-                            Spacer()
-                            Text(selectedContent?.name ?? "강의 제목")
-                                .font(.title)
-                                .fontWeight(.bold)
-                            
-                            Spacer()
-                            
-                            //  MARK: -View : 수정 버튼
-                            // TODO: EditSession 연결
-                            Button {
-                                clickedEditButton.toggle()
-                            } label: {
-                                Text("세션 수정하기")
-                                    .padding(.vertical, 13)
-                                    .padding(.trailing, 28)
-                                    .fontWeight(.semibold)
-                                    .foregroundColor(Color.accentColor)
-                                    .cornerRadius(15)
-                            }
-                            .sheet(isPresented: $clickedEditButton) {
-                                EditSessionView(seminarStore: seminarStore, seminar: selectedContent ?? Seminar(id: "", image: [], name: "", date: Date(), startingTime: "", endingTime: "", category: "", location: "", locationUrl: "", hostName: "", hostImage: "", hostIntroduction: "", seminarDescription: "", seminarCurriculum: ""))                        }
-                            
+
                         }
                         
                         .frame(minHeight: 50)
                         
-                        
-                        HStack {
-                            Image(systemName: "calendar")
-                            Text(selectedContent?.createdDate ?? "2023-01-01")
-                                .font(.subheadline)
-                                .padding(.trailing, 15)
-                            Image(systemName: "mappin.and.ellipse")
-                            Text(selectedContent?.location ?? "멋쟁이사자처럼 광화문 오피스")
-                                .font(.subheadline)
-                            
-                            Spacer()
-                            
-                            Button {
-                                clickedQRButton.toggle()
-                            } label: {
-                                Text("QR 출석 체크")
-                                    .padding(.vertical, 13)
-                                    .padding(.horizontal, 30)
-                                    .fontWeight(.bold)
-                                    .foregroundColor(Color.white)
-                                    .background(Color.accentColor)
-                                    .cornerRadius(15)
-                            }
-                        }
-                        .frame(minHeight: 30)
-                        
-                        Divider()
-                            .padding(.vertical, 20)
-                        
-                        
-                        // MARK: -View : Q&A 리스트 관리
-                        // TODO: Question 데이터 연결 (댓글 내용, 시간대 띄워주기)
-                        Text("받은 Q&A")
-                            .font(.largeTitle)
-                            .fontWeight(.bold)
-                        
-//                        List(dummyQuestions, id:\.self) { question in
-//                            Text(question)
-//                                .padding()
-//                                .listRowBackground(
-//                                    RoundedRectangle(cornerRadius: 10)
-//                                        .fill(Color.secondary.opacity(0.15))
-//                                        .foregroundColor(.white)
-//                                        .padding(
-//                                            EdgeInsets(
-//                                                top: 10,
-//                                                leading: 10,
-//                                                bottom: 10,
-//                                                trailing: 10
-//                                            )
-//                                        )
-//
-//                                )
-//                                .listRowSeparator(.hidden)
-//                        }
-//                        .scrollContentBackground(.hidden)
-//                        .listStyle(PlainListStyle())
-//                        .padding(.leading, -13)
-                        
-                        
-                        
-                        Spacer()
+
+                        // MARK: -View : 오른쪽 사이드 유저 리스트
+                        SessionDetailUserList(selectedContent: selectedContent)
+                            .frame(width: geo.size.width/4.5)
+                            .padding(.trailing, 20)
                     }
                     .padding(.leading, 40)
                     .padding(.trailing, 10)
                     
                     
-                    // MARK: -View : 오른쪽 사이드 유저 리스트
-                    SessionDetailUserList(selectedContent: selectedContent)
-                        .frame(width: geo.size.width/4.5)
-                        .padding(.trailing, 20)
+                   
                 }
             }
             .sheet(isPresented: $showCameraScannerView) {
